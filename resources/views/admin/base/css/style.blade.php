f2ab7a928b67015a9e60d7f3f8678bcf36a4eed5
Burris19 <julianhernandez19@gmail.com>
2016-03-13 23:08:56 +0000

    [add] lenguaje ES for aplication

---
+++ resources/views/admin/base/css/style.blade.php
@@ -0,0 +1,21 @@
+<!-- Vendor CSS -->
+<link rel="stylesheet" href="assets/css/vendor/bootstrap/css/bootstrap.css" />
+
+<link rel="stylesheet" href="assets/css/vendor/font-awesome/css/font-awesome.css" />
+<link rel="stylesheet" href="assets/css/vendor/magnific-popup/magnific-popup.css" />
+<link rel="stylesheet" href="assets/css/vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css" />
+
+<!-- Specific Page Vendor CSS -->
+<link rel="stylesheet" href="assets/css/vendor/jquery-ui/jquery-ui.css" />
+<link rel="stylesheet" href="assets/css/vendor/jquery-ui/jquery-ui.theme.css" />
+<link rel="stylesheet" href="assets/css/vendor/bootstrap-multiselect/bootstrap-multiselect.css" />
+<link rel="stylesheet" href="assets/css/vendor/morris.js/morris.css" />
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="assets/css/stylesheets/theme.css" />
+
+<!-- Skin CSS -->
+<link rel="stylesheet" href="assets/css/stylesheets/skins/default.css" />
+
+<!-- Theme Custom CSS -->
+<link rel="stylesheet" href="assets/css/stylesheets/theme-custom.css">
