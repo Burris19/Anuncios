e100e462b5daadd0ee0a7285a4f7eb81323bffc2
Burris19 <julianhernandez19@gmail.com>
2016-03-14 05:43:11 +0000

    [add] configure traductions

--- resources/lang/en/helpers.php
+++ resources/lang/en/helpers.php
@@ -3,6 +3,10 @@
 return [
 
     'title_page' => 'Administrator',
-
+    'password'  => 'Contraseña',
+    'recoveryPassword' => 'Olvidé mi contraseña',
+    'remember' => 'Recordarme',
+    'ok'    => 'Iniciar Sesión',
+    'copyright' => 'All rights reserved'
 
 ];
