0c2681b260f36dcf75129c355234ad0b3f6e6472
Burris19 <julianhernandez19@gmail.com>
2016-03-15 06:58:55 +0000

    [add] view configure principal

---
+++ resources/views/admin/configure/principal.blade.php
@@ -0,0 +1,82 @@
+<header class="page-header">
+    <h2>Administración</h2>
+
+    <div class="right-wrapper pull-right">
+        <ol class="breadcrumbs">
+            <li>
+                <a href="index.html">
+                    <i class="fa fa-home"></i>
+                </a>
+            </li>
+            <li><span>Configuración principal</span></li>
+        </ol>
+
+        <a class="sidebar-right-toggle" data-open="#"><i class="fa fa-chevron-left"></i></a>
+    </div>
+</header>
+<!-- start: page -->
+<div class="row">
+    <div class="col-md-12">
+        <section class="panel">
+            <header class="panel-heading">
+                <div class="panel-actions">
+                    <a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
+                </div>
+                <h2 class="panel-title">Configuración principal</h2>
+            </header>
+            <div class="panel-body">
+                <div class="row left">
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="inputDefault">Texto superior:</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" id="inputDefault">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">E-mail:</label>
+                        <div class="col-md-6">
+                            <div class="input-group mb-md">
+														<span class="input-group-addon">
+															<i class="fa fa-envelope"></i>
+														</span>
+                                <input type="text" class="form-control" placeholder="E-mail">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Teléfono:</label>
+                        <div class="col-md-6">
+                            <div class="input-group mb-md">
+														<span class="input-group-addon">
+															<i class="fa fa-phone"></i>
+														</span>
+                                <input type="text" class="form-control" placeholder="Teléfono">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="textareaDefault">Descripción Destacadas:</label>
+                        <div class="col-md-6">
+                            <textarea class="form-control" rows="3" id="textareaDefault"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="textareaDefault">Descripción Novedades:</label>
+                        <div class="col-md-6">
+                            <textarea class="form-control" rows="3" id="textareaDefault"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="textareaDefault">Descripción Banners:</label>
+                        <div class="col-md-6">
+                            <textarea class="form-control" rows="3" id="textareaDefault"></textarea>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <button type="button" class="mb-xs mt-xs mr-xs btn btn-primary"><i class="fa fa-floppy-o"></i> Guardar</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
