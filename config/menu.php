b63e9603f08671f7e76e67d1d335de254a82bd99
Burris19 <julianhernandez19@gmail.com>
2016-03-16 06:56:43 +0000

    [add] struct the menu for sidebar

---
+++ config/menu.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'configure-principal' => 'Principal',
+    'configure-seo' => 'SEO',
+    'configure-column-one' => 'Links Primera Columna',
+    'configure-column-two' => 'Links Segunda Columna',
+    'configure-banner' => 'Banners',
+    'configure-slide-show' => 'Slider'
+];
