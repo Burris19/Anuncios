d8fe8af5f9d4f7355ffcb5df10265975b7a47281
Burris19 <julianhernandez19@gmail.com>
2016-03-16 06:59:06 +0000

    [add] breadcumb the aplicacion

---
+++ resources/views/admin/base/helpsLayout/breadcumb.blade.php
@@ -0,0 +1,16 @@
+<header class="page-header">
+    <h2>Administración</h2>
+
+    <div class="right-wrapper pull-right">
+        <ol class="breadcrumbs">
+            <li>
+                <a href="index.html">
+                    <i class="fa fa-home"></i>
+                </a>
+            </li>
+            <li><span>Configuración principal</span></li>
+        </ol>
+
+        <a class="sidebar-right-toggle" data-open="#"><i class="fa fa-chevron-left"></i></a>
+    </div>
+</header>
