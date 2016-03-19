6174f31c2f68349a08df3ea31f85a2ba8ffd8683
Burris19 <julianhernandez19@gmail.com>
2016-03-19 07:51:45 +0000

    [add] add values in the view

--- resources/views/admin/configure/seo.blade.php
+++ resources/views/admin/configure/seo.blade.php
@@ -9,23 +9,27 @@
                 <h2 class="panel-title">Configuración SEO</h2>
             </header>
             <div class="panel-body">
-                <div class="row left">
-                    <div class="form-group">
-                        <label class="col-md-3 control-label" for="textareaDefault">Descripción del sitio:</label>
-                        <div class="col-md-6">
-                            <textarea class="form-control" rows="3" id="textareaDefault"></textarea>
+
+                {!! Form::open(['url' => '/admin/configure-seo/' . $data->id, 'method' => 'PUT', 'class' => 'sendForm' ]) !!}
+                    <div class="row left">
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="textareaDefault">Descripción del sitio:</label>
+                            <div class="col-md-6">
+                                <textarea class="form-control" rows="3" name="siteDescription" required>{!! $data->siteDescription !!}</textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label" for="textareaDefault">Keywords:</label>
+                            <div class="col-md-6">
+                                <textarea class="form-control" rows="3" name="keywords" required>{!! $data->keywords !!}</textarea>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <button type="submit" class="mb-xs mt-xs mr-xs btn btn-primary"><i class="fa fa-floppy-o"></i> Guardar</button>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label class="col-md-3 control-label" for="textareaDefault">Keywords:</label>
-                        <div class="col-md-6">
-                            <textarea class="form-control" rows="3" id="textareaDefault"></textarea>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-                        <button type="button" class="mb-xs mt-xs mr-xs btn btn-primary"><i class="fa fa-floppy-o"></i> Guardar</button>
-                    </div>
-                </div>
+                {!! Form::close() !!}
+
             </div>
         </section>
     </div>
