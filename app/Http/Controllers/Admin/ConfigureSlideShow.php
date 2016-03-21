63e046011acff473488267a2e2266e30c5424aea
Burris19 <julianhernandez19@gmail.com>
2016-03-21 07:45:53 +0000

    [add] set setting basic controller

--- app/Http/Controllers/Admin/ConfigureSlideShow.php
+++ app/Http/Controllers/Admin/ConfigureSlideShow.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\models\sliders;
 
 class ConfigureSlideShow extends BaseController
 {
@@ -17,10 +18,27 @@ class ConfigureSlideShow extends BaseController
      */
     protected $view = 'slideshow';
 
+    protected $input = [
+        'name',
+        'URL',
+        'photo'
+    ];
 
-          protected function getModel()
+    protected $crud = 'banner';
+
+    protected $filterName = 'name';
+
+    protected $rules = [
+        'name' => 'required',
+        'URL' => 'required'
+        //'photo'
+    ];
+
+    protected $rootBase = 'configure-slide-show';
+
+    protected function getModel()
     {
-       
+        return new sliders();
     }
 
 }
