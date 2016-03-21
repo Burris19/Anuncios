5305cdb73eaa40efa21e2548d13f9cdd75a310ad
Burris19 <julianhernandez19@gmail.com>
2016-03-21 04:06:02 +0000

    [add] traductions for views

---
+++ resources/lang/en/label.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'title_column1' => 'Links Footer Column 1',
+    'name' => 'Link name',
+    'link' => 'Link',
+    'save' => 'Save',
+    'actions' => 'Actions',
+    'label1' => 'Superior Text',
+    'label2' => 'Email',
+    'label3' => 'Telephone',
+    'label4' => 'Description Outstanding',
+    'label5' => 'Description New',
+    'label6' => 'Description Banners',
+    'title_principal' => 'Main Settings',
+    'title_SEO' => 'Settings SEO',
+    'description_site' => 'Description Site',
+    'key_words' => 'Key Words',
+    'modal_edit' => 'Updating Data',
+    'save_update' => 'Save changes',
+    'cancel' => 'Cancel'    ,
+    'modal_delete' => 'Delete Data',
+    'descriptionDelete' => 'Confirmar Eliminacion?',
+    'delete' => 'Delete'
+
+
+];
---
+++ resources/lang/es/label.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'title_column1' => 'Enlaces columna 1',
+    'name' => 'Nombre del enlace',
+    'link' => 'Enlace',
+    'save' => 'Guardar',
+    'actions' => 'Acciones',
+    'label1' => 'Texto Superior',
+    'label2' => 'Correo Electronico',
+    'label3' => 'Telefono',
+    'label4' => 'Descripcion Destacada',
+    'label5' => 'Descripcion Novedades',
+    'label6' => 'Descripcion Banners',
+    'title_principal' => 'Configuracion principal',
+    'title_SEO' => 'Configuracion SEO',
+    'description_site' => 'Descripcion del sitio',
+    'key_words' => 'Palabras Claves',
+    'modal_edit' => 'Actualizando Datos',
+    'save_update' => 'Guandar Cambios',
+    'cancel' => 'Cancelar',
+    'modal_delete' => 'Eliminar Datos',
+    'descriptionDelete' => 'Confirmar eliminacion del registro',
+    'delete' => 'Eliminar'
+];
