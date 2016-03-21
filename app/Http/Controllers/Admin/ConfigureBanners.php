6a8a6d064d1daa800cf96a3053451b204ea76cca
Burris19 <julianhernandez19@gmail.com>
2016-03-21 07:28:11 +0000

    [add] set setting basic controller

--- app/Http/Controllers/Admin/ConfigureBanners.php
+++ app/Http/Controllers/Admin/ConfigureBanners.php
@@ -5,14 +5,34 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\models\banners;
 class ConfigureBanners extends BaseController
 {
     protected $module = 'configure';
+
     protected $view = 'banners';
 
-       protected function getModel()
+    protected $input = [
+        'name',
+        'URL',
+        'photo'
+    ];
+
+    protected $crud = 'banner';
+
+    protected $filterName = 'name';
+
+    protected $rules = [
+        'name' => 'required',
+        'URL' => 'required'
+        //'photo'
+    ];
+
+    protected $rootBase = 'configure-banner';
+
+    protected function getModel()
     {
-       
+       return new banners();
     }
 
 
